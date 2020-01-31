fa6a03064262303888b8af8731c3948dfd68217a
hmnshu <gami4388@stthomas.edu>
2020-01-30 19:05:10 -0600

    latest version

--- venv/smapp/maintain/views.py
+++ venv/smapp/maintain/views.py
@@ -2,21 +2,23 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_list_or_404,get_object_or_404, redirect
 from django.db.models import Count
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.urls import reverse
 from constance import config
 from .forms import NewTopicForm, PostForm
-from .models import course, topic, post, classroom
+from .models import course, topic, post, classroom, clas_sware
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 @login_required
 def courses(request):
     if request.user.is_authenticated:
-        queryset = list(course.objects.filter(course_schedule__sch_semester = config.Semester).filter(course_instructor__username=request.user))
+        queryset = list(course.objects.filter(sch_semester = config.Semester).filter(course_instructor__username=request.user))
         if request.user.is_superuser:
-            queryset = list(course.objects.filter(course_schedule__sch_semester = config.Semester).all())        
+            queryset = list(course.objects.filter(sch_semester = config.Semester).all())        
         if queryset:
             page = request.GET.get('page', 1)
             paginator = Paginator(queryset, 10)
@@ -37,7 +39,11 @@ def courses(request):
         return render(request, 'maintain/home.html')
 
 def home(request):
-    return render(request, 'maintain/home.html')
+    sware = get_list_or_404(clas_sware)
+    tag_data = ""
+    for ind, s_name in enumerate(sware):
+        tag_data = tag_data + '{id: ' + str(ind) + ', name: "' + s_name.sware_name + '"},'
+    return render(request, 'maintain/test.html', {'tag_data': tag_data})
 
 def cou_detail(request, course_id):
     cou = get_object_or_404(course, pk=course_id)
@@ -48,6 +54,16 @@ def classroom_detail(request, course_id, class_id):
     cou = get_object_or_404(course, pk=course_id)
     return render(request, 'maintain/classroom_detail.html', {'clsroom': clsroom, 'cou': cou})
 
+@method_decorator(login_required, name='dispatch')
+class SwareResultsView(ListView):
+    model = clas_sware
+    template_name = 'maintain/clas_sware_results.html'
+    paginate_by = 10  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
 
 @login_required
 def course_topics(request, course_id):
@@ -80,6 +96,11 @@ def topic_posts(request, course_id, topic_id):
 @login_required
 def course_update(request, course_id):
     cou = get_object_or_404(course, pk=course_id)
+    sware = get_list_or_404(clas_sware)
+    tag_data = ""
+    for ind, s_name in enumerate(sware):
+        tag_data = tag_data + '{id: ' + str(ind) + ', name: "' + s_name.sware_name + '"},'
+    
     user = request.user  # get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
@@ -98,7 +119,7 @@ def course_update(request, course_id):
             return redirect('topic_posts', course_id, topic_obj.pk)   # redirect to the created topic page
     else:
         form = NewTopicForm()
-    return render(request, 'maintain/course_update.html', {'cou': cou, 'form': form})
+    return render(request, 'maintain/course_update.html', {'cou': cou, 'form': form, 'tag_data': tag_data})
 
 @login_required
 def reply_topic(request, course_id, topic_id):
